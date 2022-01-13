d9796da8a2e7fce276ab029478a93b71b64f1f51
羊夏文 <yxiawen@126.com>
2022-01-13 14:29:19 +0800

    Site updated: 2022-01-13 14:29:19

---
+++ happynewyear/lib/drawString.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: yxiawen
+ * @Date: 2022-01-13 13:57:16
+ * @LastEditTime: 2022-01-13 14:18:12
+ * @LastEditors: yxiawen
+ * @Description:
+ * @FilePath: /blog/public/happynewyear/lib/drawString.js
+ */
+function drawString(ctx, text, posX, posY, textColor, fontSize, font, rotation, align, inline) {
+  var lines = text.split("\n");
+  if (!rotation) rotation = 0;
+  if (!font) font = "'serif'";
+  if (!fontSize) fontSize = 16;
+  if (!textColor) textColor = "#000000";
+  if (!align) align = "start";
+  ctx.save();
+  ctx.font = fontSize + "px " + font;
+  ctx.fillStyle = textColor;
+  ctx.textAlign = align;
+  posY = inline ? posY : posY + fontSize;
+  ctx.translate(posX, posY);
+  ctx.rotate((rotation * Math.PI) / 180);
+  for (i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], 0, i * fontSize);
+  }
+  ctx.restore();
+}
